7aa107e31b145fbeeae7f78ed6444b3b59727f60
Bitjockey <bitjockey@jackpoint.network>
2020-09-26 06:15:19 +0100

    Make GUI optional

--- Code/Server/main.py
+++ Code/Server/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
-from ui_server import Ui_server
+import argparse
+from ui_server import Ui_server 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
@@ -8,7 +9,7 @@ from PyQt5.QtGui import *
 from Server import *
 
 class MyWindow(QMainWindow,Ui_server):
-    def __init__(self):
+    def __init__(self): 
         super(MyWindow,self).__init__()
         self.setupUi(self)
         self.pushButton_On_And_Off.clicked.connect(self.on_and_off_server)
@@ -48,9 +49,39 @@ class MyWindow(QMainWindow,Ui_server):
             pass
         QCoreApplication.instance().quit()
         os._exit(0)
+
+
+def handle_server():
+    try:
+        server = Server()
+        server.turn_on_server()
+        server.tcp_flag=True
+        video=threading.Thread(target=server.transmission_video)
+        video.start()
+        instruction=threading.Thread(target=server.receive_instruction)
+        instruction.start()
+    except KeyboardInterrupt:
+        try:
+            stop_thread(video)
+            stop_thread(instruction)
+        except Exception as e:
+            print(e)
+        server.turn_off_server()
+
+
+def main(args):
+    if args.gui:
+        app = QApplication(sys.argv)
+        myshow = MyWindow()
+        myshow.show();  
+        sys.exit(app.exec_())
+    else:
+        handle_server()
+
         
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    myshow=MyWindow()
-    myshow.show();  
-    sys.exit(app.exec_())
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--gui", action="store_true", help="Start GUI")
+    args = parser.parse_args()
+    main(args)
+
